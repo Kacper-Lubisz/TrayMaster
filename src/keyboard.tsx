c9e464906e2605462626d31ffba69ca84fcc9121
Barnaby Collins <barnabyc@sky.com>
2019-12-05 19:14:03 +0000

    Create KeyboardButton object

--- src/keyboard.tsx
+++ src/keyboard.tsx
@@ -1,3 +1,10 @@
 import React from "react";
 import "styles/shelfview.scss";
 
+// TODO: stop WebStorm getting angry about me using capitalised names for React function components -
+//  I think I'm in the right here
+
+function KeyboardButton(props: any) {
+  return (<button className="key-btn" onClick={props.onClick}>{props.name}</button>);
+}
+
