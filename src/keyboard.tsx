ccadfbf70c63827dff0c71af88519be7cdbf2f38
Barnaby Collins <barnabyc@sky.com>
2019-12-05 19:18:57 +0000

    Add interface for KeyboardButton props

--- src/keyboard.tsx
+++ src/keyboard.tsx
@@ -4,7 +4,12 @@ import "styles/shelfview.scss";
 // TODO: stop WebStorm getting angry about me using capitalised names for React function components -
 //  I think I'm in the right here
 
-function KeyboardButton(props: any) {
+interface KeyboardButtonProps {
+  name: string,
+  onClick: any // TODO: check that onClick should be any and not void or something
+}
+
+function KeyboardButton(props: KeyboardButtonProps) {
   return (<button className="key-btn" onClick={props.onClick}>{props.name}</button>);
 }
 
