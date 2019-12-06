e8b1c153e009624d64a8806f1db400d468d441ce
Barnaby Collins <barnabyc@sky.com>
2019-12-06 00:29:39 +0000

    Make keyboard component

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+//import App from './App';
+import Keyboard from "./keyboard";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
+
+let buttons = [];
+for (let i = 0; i < 40; i++) {
+  buttons.push(["Beans", i]);
+}
+
+ReactDOM.render(<Keyboard buttons={buttons} width={8}/>, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/keyboard.tsx
+++ src/keyboard.tsx
@@ -1,15 +1,53 @@
 import React from "react";
-import "styles/shelfview.scss";
+import "./styles/shelfview.scss";
 
-// TODO: stop WebStorm getting angry about me using capitalised names for React function components -
-//  I think I'm in the right here
 
 interface KeyboardButtonProps {
   name: string,
   onClick: any // TODO: check that onClick should be any and not void or something
 }
 
-function KeyboardButton(props: KeyboardButtonProps) {
-  return (<button className="key-btn" onClick={props.onClick}>{props.name}</button>);
+interface KeyboardProps {
+  buttons: any[][], // TODO: check type
+  width: number
 }
 
+function KeyboardButton(props: KeyboardButtonProps) {
+  return (
+    <button className="key-btn" onClick={props.onClick}>{props.name}</button>
+  );
+}
+
+// TODO: stop WebStorm getting angry about me using capitalised names for React function components -
+//  I think I'm in the right here
+class Keyboard extends React.Component<KeyboardProps> {
+  handleClick(i: number) {
+    alert(i);
+  }
+
+  generateBoard() {
+    const rowCount: number = Math.ceil(this.props.buttons.length / this.props.width);
+
+    let rows: any[] = [];
+    for (let i = 0; i < rowCount; i++) {
+      let btns: any[] = [];
+      for (let j = 0; j < this.props.width; j++) {
+        let buttonInfo: any[] = this.props.buttons[i * this.props.width + j];
+        btns.push(<KeyboardButton name={buttonInfo[0]} onClick={() => this.handleClick(buttonInfo[1])}/>);
+      }
+      rows.push(<div className="kb-row">{btns}</div>);
+    }
+    return rows;
+  }
+
+  render() {
+
+    return (
+      <div className="keyboard">
+        {this.generateBoard()}
+      </div>
+    );
+  }
+}
+
+export default Keyboard;
