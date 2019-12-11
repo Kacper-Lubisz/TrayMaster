857cea887fb4348d900d3a1ee83050c7ed2ea16e
mwellander <matilda.wellander@hotmail.com>
2019-12-11 11:42:58 +0000

    base menu and settings, no functionality

---
+++ src/MainMenu.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./styles/shelfview.scss";
+
+export class MainMenu extends React.Component<any, any> {
+
+
+  render() {
+    return (
+  <p>Hello</p>
+    );
+  }
+
+}
