9f4bdaae025b701c6b6c4dd4f0910ec98f3a5e76
mwellander <matilda.wellander@hotmail.com>
2019-12-13 13:01:11 +0000

    error in navigation added

---
+++ src/Error.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+
+export class Error extends React.Component<any, any> {
+
+
+  render() {
+    return (
+      <div>
+    <p>Page not found</p>
+      </div>
+    );
+  }
+
+}
