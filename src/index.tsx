8469cd9b0f63da329245ccc972ac9617c6ec018b
Barnaby Collins <barnabyc@sky.com>
2019-12-17 19:38:23 +0000

    Replace index.css, make expiries break into a new line when too long, fix button overflow
    Address #12, partially address #9

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./styles/index.css";
-import App from "./App";
+import "./styles/global.scss";
 import * as serviceWorker from "./serviceWorker";
-
+import App from "./App";
 
 ReactDOM.render(<App/>, document.getElementById("root"));
 
