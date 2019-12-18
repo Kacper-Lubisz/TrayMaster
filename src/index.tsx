e2dfbef05fa6394a2e8e5d68bc788c18f07d54ce
Barnaby Collins <barnabyc@sky.com>
2019-12-18 20:36:31 +0000

    Finalise for merge into staging

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/global.scss";
-import * as serviceWorker from "./serviceWorker";
 import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App/>, document.getElementById("root"));
 
