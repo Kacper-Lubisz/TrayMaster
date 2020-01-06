1da4541ee2e8d248545ca98f49239a524dcebd0d
Barnaby Collins <barnabyc@sky.com>
2020-01-06 13:40:45 +0000

    Add logo to loading and main menu screens
    (for some reason WebStorm hates my SVGs)

--- src/Loading.tsx
+++ src/Loading.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./styles/loading.scss";
+import logoSkew from "./Logo_skew.svg";
 
 // VARIABLES DEFINING THE TRAY SPINNER GRID
 // DO NOT ADJUST THESE WITHOUT ADJUSTING THE CORRESPONDING VALUES IN "loading.scss"
@@ -121,7 +122,7 @@ export class LoadingPage extends React.Component<any, LoadingPageState> {
         return (
             <div id="loadingPage">
                 <div id="menu-header">
-                    <h1>Shelfmaster</h1>
+                    <img alt="TrayMaster logo" src={logoSkew}/>
                 </div>
                 <div id="loading-box">
                     <svg id="spinner">
