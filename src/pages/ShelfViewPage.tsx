3b45a7ea14c057041d2bc7d3dbaf435b80518cec
Barnaby Collins <barnabyc@sky.com>
2020-01-19 20:23:43 +0000

    Fix a few issues from #56

--- src/pages/ShelfViewPage.tsx
+++ src/pages/ShelfViewPage.tsx
@@ -581,7 +581,7 @@ class ShelfViewPage extends React.Component<RouteComponentProps & ShelfViewProps
 
     /**
      * Sets the selection of all trays and tray spaces in the shelf.  If
-     * @param select if all should be  selected or deslected
+     * @param select if all should be selected or deselected
      */
     selectAll(select: "none" | "trays" | "all"): void {
         if (this.state.currentView instanceof Shelf) {
@@ -631,7 +631,7 @@ class ShelfViewPage extends React.Component<RouteComponentProps & ShelfViewProps
             if (this.state.currentView instanceof Zone) {
                 return this.state.currentView.color;
             } else if (this.state.currentView instanceof Shelf) {
-                return this.state.currentView.parentZone.color ?? "#ffffff";
+                return this.state.currentView.parentZone.color;
             } else {
                 return "#ffffff";
             }
@@ -663,39 +663,40 @@ class ShelfViewPage extends React.Component<RouteComponentProps & ShelfViewProps
                                         icon: tickRegular,
                                         onClick: this.selectAll.bind(this, "trays")
                                     };
-                            } else if (selected.length === this.getTrayCells().length) {
-                                return {
-                                    name: "Deselect All",
-                                    icon: tickSolid,
-                                    onClick: this.selectAll.bind(this, "none")
-                                };
-                            } else {
-                                return {
-                                    name: "Select All",
-                                    icon: tickSolid,
-                                    onClick: this.selectAll.bind(this, "all")
-                                };
+                                } else if (selected.length === this.getTrayCells().length) {
+                                    return {
+                                        name: "Deselect All",
+                                        icon: tickSolid,
+                                        onClick: this.selectAll.bind(this, "none")
+                                    };
+                                } else {
+                                    return {
+                                        name: "Select All",
+                                        icon: tickSolid,
+                                        onClick: this.selectAll.bind(this, "all")
+                                    };
+                                }
+                            })(),
+                            {
+                                name: "Edit Custom",
+                                icon: faCommentAlt,
+                                onClick: this.editTrayComment.bind(this),
+                                disabled: this.getSelectedTrays(false, false).length !== 1
+                            },
+                            {
+                                name: "Clear Trays",
+                                icon: faEraser,
+                                onClick: this.clearTrays.bind(this),
+                                disabled: this.getSelectedTrayCells().length === 0
+                            },
+                            { /*This code adds a button which opens a test dialog*/
+                                name: "Test Error", onClick: this.props.openDialog.bind(undefined,
+                                    App.buildErrorDialog("this is a big test", true)
+                                )
                             }
-                        })(),
-                        {
-                            name: "Edit Custom",
-                            icon: faCommentAlt,
-                            onClick: this.editTrayComment.bind(this),
-                            disabled: this.getSelectedTrays(false, false).length !== 1
-                        },
-                        {
-                            name: "Clear Trays",
-                            icon: faEraser,
-                            onClick: this.clearTrays.bind(this),
-                            disabled: this.getSelectedTrayCells().length === 0
-                        },
-                        { /*This code adds a button which opens a test dialog*/
-                            name: "Test Error", onClick: this.props.openDialog.bind(undefined,
-                                App.buildErrorDialog("this is a big test", true)
-                            )
-                        }
 
-                    ]}/>
+                        ]}
+                    />
                     <SideBar
                         zoneColor={zoneColor}
                         locationString={locationString}
