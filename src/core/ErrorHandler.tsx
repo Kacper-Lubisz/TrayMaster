4f491272e30229f2ddb27eb5f5fcdfc292f0be3e
Matilda <matilda.wellander@hotmail.com>
2020-01-20 10:44:17 +0000

    simplified the code from comments

--- src/core/ErrorHandler.tsx
+++ src/core/ErrorHandler.tsx
@@ -3,17 +3,19 @@ import {RouteComponentProps, withRouter} from "react-router-dom";
 
 
 interface ErrorHandlerState {
-    errorFound: boolean;
-    error?: Error | null;
+    error?: Error;
 }
 
 /**
-This class is wrapped around all app components in App.tsx to catch errors
+ This class is wrapped around all app components in App.tsx to catch errors
  and provide a nice UI when it happens
  Can only catch errors in components below itself in the hierarchy
 
  In development the usual React error page will be showed on top of this,
  can easily just be dismissed
+
+ NOTE: Does not catch errors in event handlers like onClick or our custom methods,
+ only errors thrown in lifecycle methods like "render"
  */
 
 class ErrorHandler extends React.Component<RouteComponentProps, ErrorHandlerState> {
@@ -21,22 +23,19 @@ class ErrorHandler extends React.Component<RouteComponentProps, ErrorHandlerStat
     constructor(props: any) {
         super(props);
         this.state = {
-            errorFound: false,
-            error: null,
+            error: undefined,
         };
     }
 
-/**
-This function catches the error and saves the information
- on what type of error in state
- */
-    componentDidCatch(error: Error): void {
+    /**
+     This function catches the error and saves the information
+     on what type of error in state
+     */
 
-        this.setState({
-            ...this.state,
-            errorFound: true,
-            error: error,
-        });
+    static getDerivedStateFromError(error: Error): ErrorHandlerState {
+        return {
+            error: error
+        };
     }
 
     /**
@@ -48,11 +47,11 @@ This function catches the error and saves the information
 
     render(): React.ReactNode {
 
-        if (this.state.errorFound) {
+        if (this.state.error) {
             return (
                 <div>
                     <h2>Something went wrong.</h2>
-                    <p>{`${this.state.error === undefined ? "?" : this.state.error}`}</p>
+                    <p>{`${this.state.error}`}</p>
                     <button className="key-btn"
                             onClick={this.exitError.bind(this)}><p>Refresh Page</p>
                     </button>
