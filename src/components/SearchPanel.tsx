56bda35ac0013ca3765c363b188a53effdc093cb
willg-c <willg-c@users.noreply.github.com>
2020-01-22 02:45:24 +0000

    Commented out code

--- src/components/SearchPanel.tsx
+++ src/components/SearchPanel.tsx
@@ -16,18 +16,23 @@ interface SearchPanelProps {
 export class SearchPanel extends React.Component<SearchPanelProps> {
 
     private toggleCategory(cat: Category): void {
+        let newCategories;
         if (this.props.search.query.categories instanceof Set) {
-            const newCategories = this.props.search.query.categories;
-            if (newCategories.has(cat)) {
-                newCategories.delete(cat);
-            } else {
-                newCategories.add(cat);
-            }
-            this.props.setQuery({
-                ...this.props.search.query,
-                categories: newCategories
-            });
+            newCategories = this.props.search.query.categories;
+        } else {
+            newCategories = new Set<Category>();
         }
+
+        if (newCategories.has(cat)) {
+            newCategories.delete(cat);
+        } else {
+            newCategories.add(cat);
+        }
+        this.props.setQuery({
+            ...this.props.search.query,
+            categories: newCategories.size ? newCategories : null
+        });
+
     }
 
     private renderCategoryOptions(): React.ReactNode {
@@ -56,11 +61,11 @@ export class SearchPanel extends React.Component<SearchPanelProps> {
         // return DOM elements using button structures
         return (
             <div id="searchPanel">
-                This is the side panel fellas. keyboardState is "{this.props.panelState}"
-                <br/>
-                <button onClick={() => this.props.setPanelState("category")}>Category</button>
-                <button onClick={() => this.props.setPanelState("weight")}>Weight</button>
-                <br/><br/>
+                {/*This is the side panel fellas. keyboardState is "{this.props.panelState}"*/}
+                {/*<br/>*/}
+                {/*<button onClick={() => this.props.setPanelState("category")}>Category</button>*/}
+                {/*<button onClick={() => this.props.setPanelState("weight")}>Weight</button>*/}
+                {/*<br/><br/>*/}
                 {this.renderCategoryOptions()}
             </div>
         );
