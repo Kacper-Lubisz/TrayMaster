c65d8b4ff1b0caf89c079c48362e0937bb074b77
Kacper Lubisz <kacperlubisz@gmail.com>
2020-02-14 02:16:20 +0000

    Changed version label color, to make it less striking

--- src/pages/MainMenu.tsx
+++ src/pages/MainMenu.tsx
@@ -68,7 +68,9 @@ class MainMenuPage extends React.Component<RouteComponentProps & MainMenuProps> 
         ];
 
         return <div className="main-menu">
-            Version: {process.env.REACT_APP_VERSION_NUMBER} {/*todo fixme put this somewhere else*/}
+            <div
+                style={{color: "#bbbbbb"}}
+            >Version: {process.env.REACT_APP_VERSION_NUMBER} {/*todo fixme put this somewhere else*/}</div>
             <TrayMasterLogo/>
             {/*todo fixme the expiry amount ought to be derived from warehouse*/}
             {this.props.expiryAmount === 0 ? undefined : <div
