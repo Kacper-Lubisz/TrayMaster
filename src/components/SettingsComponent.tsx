565f301792786015561d1f3c8277e3ab6d3f7770
Matilda <matilda.wellander@hotmail.com>
2020-02-20 13:57:24 +0000

    not working

--- src/components/SettingsComponent.tsx
+++ src/components/SettingsComponent.tsx
@@ -6,22 +6,23 @@ type Options = {
     key: string;
 };
 
-type SettingsComponentProps =
-    | {
+type RadioButton = {
     type: "radioButton";
     label: string;
     user: User;
     get: () => boolean;
     set: (value: boolean) => void;
-}
-    | {
+};
+
+type DropDown = {
     type: "dropDown";
     label: string;
     user: User;
     options: Options[];
     get: () => string;
     set: (value: string) => void;
-};
+}
+type SettingsComponentProps = RadioButton | DropDown;
 
 
 export class SettingsComponent extends React.Component<SettingsComponentProps> {
@@ -29,40 +30,50 @@ export class SettingsComponent extends React.Component<SettingsComponentProps> {
 
     renderSetting(): React.ReactNode {
         if (this.props.type === "radioButton") {
-            return <div className="settings-setting" key={this.props.label}
-                        onClick={() => {
-                            this.props.set(!this.props.get());
-                            this.forceUpdate();
-                        }}>
+            const propsAtRender = this.props;
+            return <div
+                className="settings-setting"
+                key={this.props.label}
+                onClick={((props: RadioButton) => {
+                    props.set(!props.get());
+                    this.forceUpdate();
+                }).bind(this, this.props)}
+            >
 
                 <input
                     type="checkbox"
-                    checked={this.props.get()}
+                    checked={propsAtRender.get()}
                     onChange={async e => {
-                        this.props.set(e.target.checked);
-                        await this.props.user.stage(true, true);
+                        propsAtRender.set(e.target.checked);
+                        await propsAtRender.user.stage(true, true);
                         this.forceUpdate();
                     }}
                 />
                 <p>{this.props.label}</p>
             </div>;
         } else {
-            //const defaultOption = this.props.options.filter(option => option.key.includes(this.props.get));
+            const propsAtRender = this.props;
+            const key = this.props.get;
+            //const defaultOption = this.props.options.filter(option => option.key.includes(key));
             return <div className="settings-setting" key={this.props.label}>
                 <label>
                     <p>{this.props.label}</p>
-                    <select onChange={event => this.handleChange(event.target.value)}>
+                    <select>
                         {this.props.options.map(option =>
-                            <option value={option.key}>{option.label}</option>
+                            <option
+                                key={option.label}
+                                onClick={() => {
+                                    propsAtRender.set(option.key);
+                                    this.forceUpdate();
+                                }}
+                            >
+                                {option.label}
+                            </option>
                         )}
                     </select>
                 </label>
             </div>;
         }
-    }
-
-    handleChange(newOption: string): void {
-        this.props.set(newOption);
     }
 
     render(): React.ReactNode {
