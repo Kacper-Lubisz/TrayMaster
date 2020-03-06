01afe9c695256eb75cfca013bc24554c315d9ec0
willg-c <willg-c@users.noreply.github.com>
2020-03-06 01:49:29 +0000

    Moved version comparison to after signin. Changed signin form field to email. Added disclaimer.

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import {RouteComponentProps, withRouter} from "react-router-dom";
+import storageAvailable from "storage-available";
 import {LoadingSpinner} from "../components/LoadingSpinner";
 import {TrayMasterLogo} from "../components/TrayMasterLogo";
+import {Dialog, DialogButtons, DialogTitle} from "../core/Dialog";
 import firebase from "../core/Firebase";
 import "../styles/settings.scss";
 import "../styles/signin.scss";
+import {compareVersions} from "../utils/compareVersions";
 
 const authErrorMessages: Map<string, string> = new Map<string, string>([
     ["auth/invalid-email", "Invalid email address."],
@@ -27,12 +30,16 @@ interface SignInPageState {
     loading: boolean;
 }
 
+interface SignInPageProps {
+    openDialog: (dialog: Dialog) => void;
+}
+
 /**
  * RouteComponentProps enables the history.push to change paths
  */
-class SignInPage extends React.Component<RouteComponentProps, SignInPageState> {
+class SignInPage extends React.Component<RouteComponentProps & SignInPageProps, SignInPageState> {
 
-    constructor(props: RouteComponentProps) {
+    constructor(props: RouteComponentProps & SignInPageProps) {
         super(props);
         this.state = {
             loading: false
@@ -62,7 +69,7 @@ class SignInPage extends React.Component<RouteComponentProps, SignInPageState> {
                                 }));
                             }}
                             value={this.state?.emailField ?? ""}
-                            type="text"
+                            type="email"
                             placeholder={"Email"}
                         />
                         <input
@@ -84,11 +91,15 @@ class SignInPage extends React.Component<RouteComponentProps, SignInPageState> {
                 </>
                 }
             </div>
+            <div id="disclaimer">
+                This app (and its service providers including Firebase) may use cookies and similar technologies to
+                download data for storage onto your device, which improves the app's functionality and speed. You can
+                disable cookies and other data storage using your browser settings.
+            </div>
         </>;
     }
 
     private async signIn(email: string | undefined, password: string | undefined): Promise<void> {
-
 
         if (email && password) {
             try {
@@ -97,6 +108,46 @@ class SignInPage extends React.Component<RouteComponentProps, SignInPageState> {
                     loading: true,
                 }));
                 await firebase.auth.signIn(email, password);
+
+
+                if (storageAvailable("localStorage")) {
+                    const oldVersion = localStorage.getItem("VERSION_NUMBER");
+                    if (process.env.REACT_APP_VERSION !== oldVersion) {
+                        localStorage.setItem("VERSION_NUMBER", process.env.REACT_APP_VERSION ?? "");
+                        const verDiff: 1 | 0 | -1 = ((old, current) => {
+                            if (old === null) {
+                                return 1;
+                            } else if (current === undefined) {
+                                return 0;
+                            }
+                            return compareVersions(old, current);
+                        })(oldVersion, process.env.REACT_APP_VERSION);
+
+                        const message: string = (() => {
+                            if (verDiff === 1) {
+                                return `You've been updated from ${oldVersion ?? "a previous version"} to ${process.env.REACT_APP_VERSION ?? "the latest version"}!`;
+                            } else if (verDiff === -1) {
+                                return `We've rolled you back from ${oldVersion ?? "a newer version"} to ${process.env.REACT_APP_VERSION ?? "a previous version"}. This has probably been done so that we can work to iron out some bugs that you might have encountered!`;
+                            }
+                            return "Your version number has changed, but something seems to have gone wrong. Sorry!";
+                        })();
+
+                        this.props.openDialog({
+                            closeOnDocumentClick: true,
+                            dialog: (close: () => void) => {
+                                return <>
+                                    <DialogTitle title="TrayMaster Update"/>
+                                    <div className="dialogContent">
+                                        <p>{message}</p>
+                                        <DialogButtons buttons={[{name: "Thanks!", buttonProps: {onClick: close,}}]}/>
+                                    </div>
+                                </>;
+                            }
+                        });
+                    }
+                }
+
+
             } catch (e) {
                 this.setState(state => ({
                     ...state,
